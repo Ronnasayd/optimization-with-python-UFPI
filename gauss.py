71540c2b91d564d0d39e8af9cf614183ca38862a
Ronnasayd <ronnasayd@hotmail.com>
2017-09-22 10:47:46 -0300

    gauss method

--- gauss.py
+++ gauss.py
@@ -5,11 +5,10 @@ solution = np.zeros(number_variables)
 #for coluna in range(number_variables-1):
 #	for linha in range(coluna+1,number_variables):
 #		ext_matrix[linha] = ext_matrix[linha]-(ext_matrix[linha,coluna]/ext_matrix[coluna,coluna])*ext_matrix[coluna]
-#print(ext_matrix)
 [ext_matrix.__setitem__(linha,(ext_matrix[linha]-(ext_matrix[linha,coluna]/ext_matrix[coluna,coluna])*ext_matrix[coluna]))
 for coluna in range(number_variables-1) for linha in range(coluna+1,number_variables)]
 #for linha in range(number_variables-1,-1,-1):
 #	solution[linha] = ((ext_matrix[linha,-1] + sum(-ext_matrix[linha,0:number_variables]*solution))/ext_matrix[linha,linha])
 [solution.__setitem__(linha,((ext_matrix[linha,-1] + sum(-ext_matrix[linha,0:number_variables]*solution))/ext_matrix[linha,linha])) 
 for linha in range(number_variables-1,-1,-1)] 
-print('Matriz Reduzida: '),print(ext_matrix),print('Solucao: '),print(solution)
+print('Reduced Matrix:'),print(ext_matrix),print('Solution:'),print(solution)
